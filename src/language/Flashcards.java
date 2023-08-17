e35624c8d39b637034a5a9f262ef879bacb749c1
joelmacias <joelmacias@localhost>
2023-08-17 15:32:57 +1200

    alter flashcard

--- src/language/Flashcards.java
+++ src/language/Flashcards.java
@@ -12,11 +12,7 @@ public class Flashcards {
 
     public static void main(String[] args) {
 
-        int[] numbers = new int[2];
-        numbers[0] = 6;
-        numbers[1] = 7;
-        
-        System.out.println(numbers.getClass());
+        System.out.println("Hello World");
     }
 
 }
