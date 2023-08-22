70f19b4da5ef367edf6f06b29b9417735cd7b1ec
joellmacias <athnico2@gmail.com>
2023-08-22 19:27:41 +1200

    vocabitem implementation

--- src/language/VocabularyItem.java
+++ src/language/VocabularyItem.java
@@ -9,9 +9,38 @@ package language;
  * @author joelmacias
  */
 public class VocabularyItem {
-    
-    public VocabularyItem()
-    {
-        
+
+    private String word;
+    private String translation;
+    private int difficulty;
+
+    public VocabularyItem(String word, String translation, int difficulty) {
+        setWord(word);
+        setTranslation(translation);
+        setDifficulty(difficulty);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
     }
 }
