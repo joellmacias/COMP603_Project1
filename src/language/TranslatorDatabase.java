b6d42815020f0909b9e6838e7dd1578738c519f8
WUTSUPug <116047000+WUTSUPug@users.noreply.github.com>
2023-10-23 22:55:48 +1300

    populated language database. created user and language table methods

--- src/language/TranslatorDatabase.java
+++ src/language/TranslatorDatabase.java
@@ -9,11 +9,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author Bishop
  */
+
 public class TranslatorDatabase 
 {
     private static final String USER_NAME = "game";
@@ -25,15 +30,17 @@ public class TranslatorDatabase
     public TranslatorDatabase()
     {
         establishConnection();
+        createLanguageTable();
+        createUserTable();
+    }
+    
+    public static void main(String[] args) 
+    {
+        TranslatorDatabase translatorDB = new TranslatorDatabase();
+        System.out.println(translatorDB.getConnection());
         
     }
     
-   /* public static void main(String[] args) 
-    {
-        TranslatorDatabase translatordb = new TranslatorDatabase();
-        System.out.println(translatordb.getConnection());
-    }
-    */
     
     public void establishConnection() 
     {
@@ -70,6 +77,83 @@ public class TranslatorDatabase
             }
         }
     }
+    
+    public void createLanguageTable()
+    {
+        try (Statement statement = conn.createStatement())
+        {
+            String createTableSpannish = "CREATE TABLE SPANNISH(" +
+                    "WORD VARCHAR(255)" +
+                    ")";
+            statement.executeUpdate(createTableSpannish);
+            
+            String createTableSamoan = "CREATE TABLE SAMOAN(" +
+                    "WORD VARCHAR(255)" +
+                    ")";
+            statement.executeUpdate(createTableSamoan);
+            
+            String createTableMaori = "CREATE TABLE MAORI(" +
+                    "WORD VARCHAR(255)" +
+                    ")";
+            statement.executeUpdate(createTableMaori);
+            
+            System.out.println("Languages Successfully Loaded");
+            statement.close();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void readLanguage(String language)
+    {
+        try
+        {
+            String sql = "SELECT * FROM table WHERE language = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, language);
+            ResultSet resultSet = statement.executeQuery();
+            
+            while (resultSet.next())
+            {
+                String data = resultSet.getString("data_column");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public void createUserTable()
+    {
+        try (Connection conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD))
+        {
+            try (Statement statement = conn.createStatement())
+            {
+                String createTableSQL = "CREATE TABLE USERS(" +
+                         "NAME VARCHAR(15) NOT NULL, " +
+                         "SCORE VARCHAR(2) NOT NULL" +
+                         ")";
+                statement.executeUpdate(createTableSQL);
+                System.out.println("User Table successfully created");
+            }
+        }
+        catch (SQLException e)
+        {
+            if(e.getSQLState().equals("X0Y32"))
+            {
+                System.out.println("Table already exists");
+            }
+            else
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
     
     
 }
